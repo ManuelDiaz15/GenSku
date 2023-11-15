dd5da11f0bccdd2ef99fa0ad4aec40b916de3dde
Manuel <mana.diaz@duocuc.cl>
2023-11-15 02:19:49 -0300

    Agregando funcion la cual rescata los campos

--- GenSku/core/views.py
+++ GenSku/core/views.py
@@ -12,5 +12,22 @@ def history(request):
     return render (request, "core/history.html")
 
 def create_sku(request):
-    return render (request, "core/create_sku.html")
+    if request.method == 'POST':
+        # Obtener los datos del formulario
+        marca = request.POST.get('marca')
+        nombre = request.POST.get('nombre')
+        categoria = request.POST.get('categoria')
+        tipo_manejo = request.POST.get('opcionesSelect')
+        valor_numerico = request.POST.get('unikg')
 
+        # Validar y construir SKU
+        if tipo_manejo == 'unidades':
+            sku = marca + nombre + categoria + valor_numerico[:4]
+        else:
+            sku = marca + nombre + categoria + valor_numerico[:4] + 'KG'
+
+        # Renderizar la plantilla con el SKU generado
+        return render(request, 'core/create_sku.html', {'sku': sku})
+
+    # Renderizar el formulario vacío si no se ha enviado aún
+    return render(request, 'core/create_sku.html')
