3b7e9a487fd0fc0899502df37c2b06a49bca2046
Manuel <mana.diaz@duocuc.cl>
2023-11-16 01:50:07 -0300

    Generador Listo de SKU

--- GenSku/core/static/core/js/popup/popup.js
+++ GenSku/core/static/core/js/popup/popup.js
@@ -8,24 +8,3 @@ document.addEventListener('DOMContentLoaded', function () {
         procedimientoDiv.style.display = 'none';
     }, 5000); // 5000 milisegundos = 5 segundos
 });
-
-
-function generarSKU() {
-    // Implementa la lógica de generación de SKU en JavaScript si es necesario
-    // ...
-
-    // Puedes utilizar AJAX para manejar la respuesta del servidor y mostrar el SKU en el formulario
-    $.ajax({
-        type: 'POST',
-        url: '{% url "generar_sku" %}',
-        data: $('#skuForm').serialize(),
-        success: function(response) {
-            $('#skugen').val(response.sku);
-        },
-        error: function(error) {
-            console.log(error);
-        }
-    });
-}
-
-
--- GenSku/core/views.py
+++ GenSku/core/views.py
@@ -11,20 +11,31 @@ def services(request):
 def history(request):
     return render (request, "core/history.html")
 
+from django.shortcuts import render, HttpResponse
+
+from django.shortcuts import render, HttpResponse
+
+from django.shortcuts import render, HttpResponse
+
 def create_sku(request):
     if request.method == 'POST':
         # Obtener los datos del formulario
-        marca = request.POST.get('marca')
-        nombre = request.POST.get('nombre')
-        categoria = request.POST.get('categoria')
-        tipo_manejo = request.POST.get('opcionesSelect')
-        valor_numerico = request.POST.get('unikg')
+        marca = request.POST.get('marca')[:1].upper()
+        nombre = request.POST.get('nombre')[:4].upper()
+        categoria = request.POST.get('categoria')[:3].upper()
+        opcion_unidades = request.POST.get('opcionUnidades')
+        opcion_peso = request.POST.get('opcionPeso')
+        valor_numerico = request.POST.get('unikg')[:4]
 
         # Validar y construir SKU
-        if tipo_manejo == 'unidades':
-            sku = marca + nombre + categoria + valor_numerico[:4]
+        if opcion_unidades:
+            sku_suffix = 'UNI'
+        elif opcion_peso:
+            sku_suffix = 'KG'
         else:
-            sku = marca + nombre + categoria + valor_numerico[:4] + 'KG'
+            sku_suffix = ' ERROR INGRESA VALOR (UNI / KG) '  # Si ninguna opción está seleccionada
+
+        sku = marca + nombre + categoria + valor_numerico + sku_suffix
 
         # Renderizar la plantilla con el SKU generado
         return render(request, 'core/create_sku.html', {'sku': sku})
