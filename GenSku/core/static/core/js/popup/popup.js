cf4f2b5f1ad82003990811f0d75a7b343780722d
Manuel <mana.diaz@duocuc.cl>
2023-11-16 16:00:39 -0300

    Validar numero enteros

--- GenSku/core/static/core/js/popup/popup.js
+++ GenSku/core/static/core/js/popup/popup.js
@@ -8,3 +8,15 @@ document.addEventListener('DOMContentLoaded', function () {
         procedimientoDiv.style.display = 'none';
     }, 5000); // 5000 milisegundos = 5 segundos
 });
+function validarNumero(input) {
+    // Eliminar cualquier carácter no numérico
+    input.value = input.value.replace(/[^0-9]/g, '');
+
+    // Mostrar un mensaje de error si no se ingresa un número
+    var mensajeError = document.getElementById("mensajeError");
+    if (isNaN(input.value)) {
+        mensajeError.innerHTML = "Por favor, ingresa solo valores numéricos.";
+    } else {
+        mensajeError.innerHTML = "";
+    }
+}
